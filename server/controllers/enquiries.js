afe8365b5d03fbc38e565f152d38c85ee735c037
bertinNDAYISHIMIYE <72761713+bertinNDAYISHIMIYE@users.noreply.github.com>
2020-12-09 11:07:59 +0200

    ft-findOne-enquiry add finding one enquiry route add finding one enquiry logic into controllers []

--- server/controllers/enquiries.js
+++ server/controllers/enquiries.js
@@ -41,4 +41,29 @@ export class enquiriescontrollers {
   };
 
 
+  static findOneEnquiry = async (req, res) => {
+    try {
+      const enquiry = await enquiries.findOne({ _id: req.params.id });
+      if (!enquiry) {
+        return res.status(404).json({
+          status: 404,
+          message: "enquiry doesn't exist",
+        });
+      } else {
+        return res.status(200).json({
+          status: 200,
+          message: "enquiry found",
+          data: enquiry,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        status: 500,
+        message: "server error",
+      });
+    }
+  };
+  
+
  }
