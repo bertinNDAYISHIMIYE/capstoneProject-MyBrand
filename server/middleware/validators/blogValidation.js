908556f39f0502cf73a98f7e5179005c85fdfb03
bertinNDAYISHIMIYE <72761713+bertinNDAYISHIMIYE@users.noreply.github.com>
2020-12-10 10:45:10 +0200

    No longer limit user on content words

--- server/middleware/validators/blogValidation.js
+++ server/middleware/validators/blogValidation.js
@@ -5,10 +5,10 @@ import {base_response} from '../../helper/joiResponse.js'
 export class blogValidation{
 static blogvalidation = async (req, res, next) =>{
     const schema = Joi.object({
-      title: Joi.string().min(3).max(30).required(),
-      author: Joi.string().min(3).max(30).required(),
+      title: Joi.string().min(3).max(1500).required(),
+      author: Joi.string().min(3).max(100).required(),
       image: Joi.string(),
-      content: Joi.string().min(2).max(300).required()
+      content: Joi.string().min(2).required()
     });
     base_response(req, res, schema, next);
   
