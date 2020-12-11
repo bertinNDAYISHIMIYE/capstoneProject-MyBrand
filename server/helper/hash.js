fb45da6742fb9f5c3051ccaf6807a48f8381029b
bertinNDAYISHIMIYE <72761713+bertinNDAYISHIMIYE@users.noreply.github.com>
2020-12-11 22:14:43 +0200

    ft-create-user add create user route add user modal add add middleware to hash user password add middleware to validate inputs add create user logic into controllers []

---
+++ server/helper/hash.js
@@ -0,0 +1,10 @@
+
+import bcrypt from 'bcryptjs';
+
+export const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(8);
+    
+    const hashPassword = await bcrypt.hash(password, salt);
+    
+    return hashPassword;
+};
