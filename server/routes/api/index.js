1052fb7a52bb4873a9ba30c41b34c0cab322f376
bertinNDAYISHIMIYE <72761713+bertinNDAYISHIMIYE@users.noreply.github.com>
2020-12-12 14:02:54 +0200

    ft-post-comment add comment route add comment validating middleware add comment logic into controllers []

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,6 +2,7 @@ import Router from 'express';
 import blogsRoutes from './blogs.js';
 import enquiryRoutes from './enquiryRoutes.js';
 
+import blogsCommentsRoutes from './blogscomments.js';
 
 
 const router = Router();
@@ -9,4 +10,5 @@ const router = Router();
     router.use("/",enquiryRoutes);
     router.use("/blogs", blogsRoutes);
 
+    router.use("/blogs/comments", blogsCommentsRoutes);
     export default router;
